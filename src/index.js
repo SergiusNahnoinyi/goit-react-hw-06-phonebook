9a61b5c852f5153e4479bb775b16c1e35729e8a7
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-03-21 15:22:11 +0200

    Создает хранилище в Local Storage с помощью configureStore()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import App from './App';
+import { store, persistor } from './components/redux/store';
+
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
